71734069fe5931506502bf3278f5bc4c48bc7f2a
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2023-08-27 20:21:09 +0100

    CD script with npm & pm2 exposed on ('/cd-deploy')

---
+++ cd-scripts/deploy.sh
@@ -0,0 +1,8 @@
+####################################################
+##### Change this file to fit your CD steps ########
+####################################################
+
+#pull resent git commits
+git pull && \
+# restart the server
+npm run restart
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const { exec } = require('child_process');
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
     res.send("APP RUNNG");
+});
+
+app.post('/cd-deploy', (req, res) => {
+    exec('sh ./cd-scripts/deploy.sh', (error, stdout, stderr) => {
+        stdout ? console.log("stdout: ", stdout) : '';
+        stderr ? console.log("stderr: ", stderr) : '';
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    });
+    res.send("Ok");
 });
 
 app.listen(port, () => {
